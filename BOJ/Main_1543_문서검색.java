11c145f2a89c121326dbc666cfaa2ac6d1e075b2
Hamos17 <gksmfwm1000@gmail.com>
2023-07-23 23:37:18 +0900

    String

--- BOJ/Main_1543_문서검색.java
+++ BOJ/Main_1543_문서검색.java
@@ -4,11 +4,13 @@ import java.io.InputStreamReader;
 public class Main_1543_문서검색 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String doc = br.readLine();
 		String word = br.readLine();
 		
+		int length = word.length();
 		int count = 0;
+
 		while(true) {
 			int idx = doc.indexOf(word);
 			
@@ -17,7 +19,8 @@ public class Main_1543_문서검색 {
 			}
 			
 			count++;
-			doc = doc.substring(idx + word.length());
+
+			doc = doc.substring(idx + length);
 		}
 		
 		System.out.print(count);
