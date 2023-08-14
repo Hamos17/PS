767f9b6c92b84eb50efaac60d1872de9a2ea94b1
Hamos17 <gksmfwm1000@gmail.com>
2023-08-14 19:01:36 +0900

    사칙연산

---
+++ Programmers/L0/Solution_L0_몫구하기.java
@@ -0,0 +1,11 @@
+
+public class Solution_L0_몫구하기 {
+	public static int solution(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solution(10, 5));
+		System.out.println(solution(7, 2));
+	}
+}
