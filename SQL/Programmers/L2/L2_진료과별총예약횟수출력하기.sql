75ff85bd4fa814f1011bad4c977eaae3acb6c7ac
Hamos17 <gksmfwm1000@gmail.com>
2023-09-16 02:01:25 +0900

    GROUP BY

---
+++ SQL/Programmers/L2/L2_진료과별총예약횟수출력하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT MCDP_CD             AS 진료과코드
+     , COUNT(MCDP_CD)      AS "5월예약건수"
+FROM   APPOINTMENT
+WHERE  1 = 1 
+AND    TO_CHAR(APNT_YMD, 'YYYY-MM-DD') >= '2022-05-01'
+AND    TO_CHAR(APNT_YMD, 'YYYY-MM-DD') < '2022-06-01'
+GROUP BY MCDP_CD
+ORDER BY 2, 1;
