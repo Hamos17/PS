e1ea60a6127c07cfef08893616c28c4a3173dfed
Hamos17 <gksmfwm1000@gmail.com>
2023-12-22 12:29:42 +0900

    구현

---
+++ BOJ/Main_9498_시험성적.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_9498_시험성적 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int score = Integer.parseInt(st.nextToken());
+
+		if(90 <= score && score <= 100) {
+			System.out.println("A");
+		}else if(80 <= score && score <= 89) {
+			System.out.println("B");
+		}else if(70 <= score && score <= 79){
+			System.out.println("C");
+		}else if(60 <= score && score <= 69) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+	}
+}
